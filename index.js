1ba937baed6564a3babeca9be1af4f83b9618a3e
seron <seroncj@gmail.com>
2022-09-06 10:25:05 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  for (let m = 0; m < word.length / 2; m++) {
+    const n = word.length - 1 - m;
+    if (word[m] !== word[n]) {
+      return false;
+    }
+  }
+
+  return true;
 }
+
 
 /* 
   Add your pseudocode here
@@ -8,11 +16,16 @@ function isPalindrome(word) {
 
 /*
   Add written explanation of your solution here
+  //It iterate from  start to the middle while comparing 
+  it  should return same and false if not
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("racecar"));
+  console.log("");
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
@@ -21,5 +34,4 @@ if (require.main === module) {
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
 }
-
-module.exports = isPalindrome;
+module.exports=isPalindrome;
